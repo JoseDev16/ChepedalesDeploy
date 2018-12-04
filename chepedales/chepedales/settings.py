bf769295d8d166e5d0a4a3fcacdab05cb9ff8aaa
JoseDev16 <42664769+JoseDev16@users.noreply.github.com>
2018-12-04 10:20:42 -0600

    Oficial >v

--- chepedales/chepedales/settings.py
+++ chepedales/chepedales/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7$s%$fk@uv*$t1f48m1mnk#!qt$j#a%5*1k##94u93k$y(pp*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
